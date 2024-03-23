3675d8f5b2000d74c9b3416d53c5e9ba422c2dc4
Aluerie❤ <aluerie@outlook.com>
2024-03-23 06:32:14 +0300

    Remove WeaponWheelSettings

--- ShiftArt/Graphics/CGraphics.h
+++ ShiftArt/Graphics/CGraphics.h
@@ -18,5 +18,3 @@ ID3D11RenderTargetView* mainRenderTargetView;
 LRESULT __stdcall WndProc(const HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 HRESULT __stdcall hkPresent(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags);
 extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-
-void WeaponWheelSettings();
